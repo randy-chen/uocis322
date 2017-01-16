849240cf5b5752f33cf1dcc658f9b0340b6318d1
Randy Chen <randy.chen96@gmail.com>
2017-01-16 07:35:17 +0000

    Final version of script after thorough testing

--- install_daemons.sh
+++ install_daemons.sh
@@ -4,7 +4,7 @@ cd $HOME
 git clone https://github.com/postgres/postgres.git 
 cd postgres
 git checkout -b REL9_5_STABLE origin/REL9_5_STABLE
-./configure --prefix=$HOME/installed    
+./configure --prefix=/home/osnapdev/installed    
 make 
 make install
 
@@ -12,6 +12,6 @@ cd $HOME
 curl http://www.gtlib.gatech.edu/pub/apache//httpd/httpd-2.4.25.tar.bz2 > httpd-2.4.25.tar.bz2
 tar -xjf httpd-2.4.25.tar.bz2
 cd httpd-2.4.25
-./configure --prefix=$HOME/installed
+./configure --prefix=/home/osnapdev/installed
 make
 make install
