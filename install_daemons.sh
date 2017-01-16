87e1d133a9fc0e916f5643ef951c81f7ad9a71b5
Randy Chen <randy.chen96@gmail.com>
2017-01-16 04:08:03 +0000

    Script can now install correct version of postgres

---
+++ install_daemons.sh
@@ -0,0 +1,11 @@
+# Randy Chen, CIS 322 Assignment 1.
+
+git clone https://github.com/postgres/postgres.git 
+cd postgres
+git checkout -b REL9_5_STABLE origin/REL9_5_STABLE
+./configure --prefix-$HOME/osnapdev/installed    
+make 
+make install
+# use curl to download Apache httpd-2.4.25
+# configure/make/install httpd
+#
