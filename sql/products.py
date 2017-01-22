f0c6ddc1e72e87bb986c1c9cc9bb630ea2c188a1
Randy Chen <randy.chen96@gmail.com>
2017-01-22 08:48:45 +0000

    Constructing the Products table

---
+++ sql/products.py
@@ -0,0 +1,18 @@
+# Thanks to Andrew Hampton for the starter code.
+
+import csv
+
+def products():
+
+	with open('/home/osnapdev/uocis322/osnap_legacy/product_list.csv', 'r') as csvfile, open('/home/osnapdev/uocis322/osnap_legacy/products.csv', 'w') as outfile:
+		title  = ['vendor', 'description']
+		reader = csv.DictReader(csvfile)
+		writer = csv.DictWriter(outfile, fieldnames=title)
+		writer.writerheader()
+		
+		for row in reader:
+			writer.writerow({'vendor':row['vendor'], 'description':row['description']})
+
+	return
+
+products()
---
+++ sql/testscript.py
@@ -0,0 +1,10 @@
+# Randy Chen, CIS 322 Assignment 2.
+
+import psycopg2
+import csv
+
+csv_data = csv.reader(file('<filename>.csv'))
+
+dab = psycopg2.connect( dab = "lost")
+
+
