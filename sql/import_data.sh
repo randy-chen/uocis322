2d2c0f5f1993bde7f72d1759e79fcde18823c76a
Randy Chen <randy.chen96@gmail.com>
2017-01-22 10:21:31 +0000

    more changes to import_data.sh

--- sql/import_data.sh
+++ sql/import_data.sh
@@ -3,4 +3,38 @@
 # database = $1
 # portnum  = $2
 
- 
+# curl https://classes.cs.uoregon.edu//17W/cis322/files/osnap_legacy.tar.gz > legacy.tar.gz 
+# tar -xzvf legacy.tar.gz
+
+## now osnap_legacy is a directory of csv files in our directory.
+# use psql to import everything into the database??
+# waiwaiwait, what about the "psql -c "COPY tbname FROM 'PATH/products.csv' delimiter ......" " ????
+
+python3 insert1.py > tmp.sql # put insertion statements into sql file.
+
+psql $1 -p $2 -f tmp.sql     
+
+python3 insert1.py > tmp.sql 
+
+psql $1 -p $2 -f tmp.sql 
+
+python3 insert1.py > tmp.sql 
+
+psql $1 -p $2 -f tmp.sql 
+
+python3 insert1.py > tmp.sql 
+
+psql $1 -p $2 -f tmp.sql 
+
+python3 insert1.py > tmp.sql 
+
+psql $1 -p $2 -f tmp.sql 
+
+python3 insert1.py > tmp.sql 
+
+psql $1 -p $2 -f tmp.sql 
+
+python3 insert1.py > tmp.sql 
+
+psql $1 -p $2 -f tmp.sql 
+
