911e8fb1d197476760f4b20956132bcce53ccdf2
Randy Chen <randy.chen96@gmail.com>
2017-01-23 04:01:36 +0000

    scripts complete, need testing

--- sql/import_data.sh
+++ sql/import_data.sh
@@ -4,43 +4,34 @@
 
 # curl https://classes.cs.uoregon.edu//17W/cis322/files/osnap_legacy.tar.gz > legacy.tar.gz 
 # tar -xzvf legacy.tar.gz
-# psql $1 -f create_tables.sql
+psql $1 -f create_tables.sql
 ## now osnap_legacy is a directory of csv files in our directory.
 
 python3      products.py $1 $2
 python3        assets.py $1 $2
-python3     vechicles.py $1 $2
+#python3     vechicles.py $1 $2
 python3    facilities.py $1 $2
 python3      asset_at.py $1 $2
+
+
+COMMENT2=false
+if ${COMMENT2}; then
 python3       convoys.py $1 $2
 python3       used_by.py $1 $2
 python3      asset_on.py $1 $2
 
-COMMENT2=false
-if ${COMMENT2}; then
 python3         users.py $1 $2
 python3         roles.py $1 $2
 python3       user_is.py $1 $2
 python3 user_supports.py $1 $2
-fi
 
 python3        levels.py $1 $2
 python3  compartments.py $1 $2
 python3 security_tags.py $1 $2
-
+fi
 
 
 
 # rm somthing
 
 
-COMMENT=false
-if ${COMMENT}; then
-python3 insert1.py > tmp.sql # put insertion statements into sql file.
-
-psql $1 -p $2 -f tmp.sql     
-
-python3 insert1.py > tmp.sql 
-
-psql $1 -p $2 -f tmp.sql 
-fi
