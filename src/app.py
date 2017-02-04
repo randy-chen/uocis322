32d24bce5b7f25ddf07e39be7c24c0dfcb9ecb32
Randy Chen <randy.chen96@gmail.com>
2017-02-04 11:34:35 +0000

    Database queries executing and showing on report pages

--- src/app.py
+++ src/app.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 from config import dbname, dbhost, dbport
+import psycopg2
 
 app = Flask(__name__)
+print(dbname, dbhost, dbport)
+conn = psycopg2.connect(dbname=dbname, host=dbhost, port=dbport)
+cur  = conn.cursor()
+
 
 @app.route('/')
 def index():
@@ -23,34 +28,46 @@ def rfs():
 
 @app.route('/facilities')
 def facilities():
-    if request.method=='GET' and 'facilities' in request.args:
-        return render_template('facilities.html',data=request.args.get('facilities'))
+	sql = """select fcode, asset_tag, arrive_dt, depart_dt
+from assets a
+join asset_at aa on a.asset_pk=aa.asset_fk
+join facilities f on aa.facility_fk=f.facility_pk
+where aa.arrive_dt < %s and (aa.depart_dt is NULL or aa.depart_dt > %s) """
+	arrive_time = (request.args.get('facilities'), request.args.get('facilities'),)
+	cur.execute(sql, arrive_time)
+	res = cur.fetchall()
+	session['result'] = res
 
-    # request.form is only populated for POST messages
-    #if request.method=='POST' and 'login' in request.form:
-    #    return render_template('rfs.html',data=request.form['login'])
-    return render_template('index.html')
+	if request.method=='GET' and 'facilities' in request.args:
+		return render_template('facilities.html',data=request.args.get('facilities'))
+
+	return render_template('index.html')
 
 @app.route('/transit')
 def transit():
-    if request.method=='GET' and 'transit' in request.args:
-        return render_template('transit.html',data=request.args.get('transit'))
+	sql = """select request_id, asset_tag, load_dt, unload_dt
+from assets a
+join asset_on aa on a.asset_pk=aa.asset_fk
+join convoys c on aa.convoy_fk=c.convoy_pk
+where aa.load_dt < %s and (aa.unload_dt is NULL or aa.unload_dt > %s) """
+	load_time = (request.args.get('transit'), request.args.get('transit'),)
+	cur.execute(sql, load_time)
+	res = cur.fetchall()
+	session['result'] = res
+	
+	if request.method=='GET' and 'transit' in request.args:
+		return render_template('transit.html',data=request.args.get('transit'))
 
-    # request.form is only populated for POST messages
-    #if request.method=='POST' and 'login' in request.form:
-    #    return render_template('rfs.html',data=request.form['login'])
-    return render_template('index.html')
+	return render_template('index.html')
 
 @app.route('/logout')
 def logout():
-    if request.method=='GET':
-        return render_template('logout.html')
+	if request.method=='GET':
+		return render_template('logout.html')
 
-    # request.form is only populated for POST messages
-    #if request.method=='POST' and 'login' in request.form:
-    #    return render_template('rfs.html',data=request.form['login'])
-    return render_template('index.html')
+	return render_template('index.html')
 
 
 if __name__ == "__main__":
+	app.secret_key = "hello"
 	app.run(host='0.0.0.0', port=8080)
